2ffe1eea1ee32f07f9ae5b23a68cb596b9ebaf33
jerobejrobinson <jerobejrobinson@gmail.com>
2024-06-25 14:12:50 -0500

    mvp v3

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ import SignoutBtn from "./SignoutBtn"
 export default function Nav() {
     return (
         <nav className="flex items-center xl:items-start justify-center gap-4 xl:flex-col h-full xl:py-4" >
-            <Link href={'/dashboard'} className="xl:pl-4">Dashboard</Link>
+            {/* <Link href={'/dashboard'} className="xl:pl-4">Dashboard</Link> */}
             <Link href={'/dashboard/claims'} className="xl:pl-4">Claims</Link>
             {/* <Link href={'/dashboard/account'} className="xl:pl-4">Account</Link> */}
             <SignoutBtn />
